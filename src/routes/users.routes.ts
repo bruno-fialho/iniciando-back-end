99467e0dcad747cbf0a989100b3a267ced17d269
bruno-fialho <brugerf@gmail.com>
2020-09-17 18:45:31 -0300

    Images upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import UserMap from '../mappers/UserMap';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 	try {
@@ -27,5 +33,27 @@ usersRouter.post('/', async (request, response) => {
 		return response.status(400).json({ error: err.message });
 	}
 });
+
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (request, response) => {
+		try {
+			const updateUserAvatar = new UpdateUserAvatarService();
+
+			const user = await updateUserAvatar.execute({
+				user_id: request.user.id,
+				avatarFilename: request.file.filename,
+			});
+
+			const mappedUser = UserMap.toDTO(user);
+
+			return response.json({ mappedUser });
+		} catch (err) {
+			return response.status(400).json({ error: err.message });
+		}
+	},
+);
 
 export default usersRouter;
